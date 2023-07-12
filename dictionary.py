02f1fd6f53ca92a8ebb26dd0a14adb3b0c4316c4
Rohan Gohil <124257389+rohanhgohil@users.noreply.github.com>
2023-07-12 10:13:52 +0530

    Uploaded file

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dictionary = {
+    'int':'Integer data type',
+    'float':'Decimanl data type',
+    'str':'String data type',
+    'list':'It contains collection of mutable elements',
+    'tup':'It contains collection of immutable element',
+    'set':'It is collection of non repeated elements',
+    'dict':'It is collection of key:value pairs'
+}
+user = input("Enter your data type: ")
+print(dictionary[user])
